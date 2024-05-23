94d508d4edd41297226427f86cdc6877a7a670a6
A-MOHAMED14 <amin800@hotmail.co.uk>
2024-05-23 13:31:10 +0100

    Add comments describing the functionality of the code found in Unsolved.

--- 03-JavaScript/01-Activities/26-Stu_Object-This/Unsolved/script.js
+++ 03-JavaScript/01-Activities/26-Stu_Object-This/Unsolved/script.js
@@ -1,28 +1,32 @@
 // TODO: What does 'this' refer to?
+// A: Refers to the window
 console.log(this);
 
 // TODO: What does 'this' refer to?
-function helloThis() { 
-   console.log("Inside this function, this is " + this); 
-   }
+// A: Refers to the global object
+function helloThis() {
+  console.log("Inside this function, this is " + this);
+}
 
-// TODO: What will this log? 
-var child = { 
-   age: 10,
-   ageTenYears: function() {
-       console.log(this.age + 10); 
-   }
+// TODO: What will this log?
+// A: Refers to the child object, will log 20
+var child = {
+  age: 10,
+  ageTenYears: function () {
+    console.log(this.age + 10);
+  },
 };
 
-// TODO: What will this log? 
-var investor = {  
+// TODO: What will this log?
+// A: Refers to the investor object, will log 5750
+var investor = {
   name: "Cash Saver",
   investment: {
     initialInvestment: 5000,
-    investmentGrowth: function() {
-      console.log(this.initialInvestment * 1.15)
-    }
-   }   
+    investmentGrowth: function () {
+      console.log(this.initialInvestment * 1.15);
+    },
+  },
 };
 
 // TODO: After commenting, check your results!
