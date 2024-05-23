1fc76c3323ee70eb46c249fc6e6d8633a3ae1f81
A-MOHAMED14 <amin800@hotmail.co.uk>
2024-05-23 13:10:41 +0100

    Log Customer's Drink Order Using an Object

--- 03-JavaScript/01-Activities/22-Stu_Objects/Unsolved/script.js
+++ 03-JavaScript/01-Activities/22-Stu_Objects/Unsolved/script.js
@@ -1,1 +1,17 @@
 //WRITE YOUR CODE BELOW
+
+const customerDrink = {
+  name: "iced latte",
+  sugars: 2,
+  isReady: true,
+};
+
+console.log(customerDrink);
+
+if (customerDrink.isReady === true) {
+  console.log(
+    `Ready for pick-up: ${customerDrink.name} with ${customerDrink.sugars} sugars.`
+  );
+} else {
+  console.log("Still in order queue...");
+}
