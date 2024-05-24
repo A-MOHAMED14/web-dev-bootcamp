71776fde94687cdc09d46b48198f38306725ef03
A-MOHAMED14 <amin800@hotmail.co.uk>
2024-05-24 17:48:18 +0100

    Add comments describing the functionality of the code found in unsolved - script.js

--- 04-Web-APIs/01-Activities/02-Stu_Window-Object/Unsolved/script.js
+++ 04-Web-APIs/01-Activities/02-Stu_Window-Object/Unsolved/script.js
@@ -1,12 +1,15 @@
 // TODO: What will the following line of code log?
 console.log(window);
+// A: Returns the window object
 
 // TODO: What will the following line of code log?
 console.log(window.document);
+// A: Logs the entire HTML document loaded in the window
 
 // TODO: What will the following line of code log?
 console.log(document.documentElement);
+// A: Logs the root element of the document
 
 // TODO: What will the following line of code log?
 console.log(document.head);
-
+// A: Logs the <head> element of the current document.
