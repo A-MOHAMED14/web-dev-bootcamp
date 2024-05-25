8917390b744ab64b1e0730aa54e10e75af90117f
A-MOHAMED14 <amin800@hotmail.co.uk>
2024-05-25 11:44:45 +0100

    Display the value and code of a pressed key

--- 04-Web-APIs/01-Activities/16-Stu_Keyboard-Events/Unsolved/assets/js/script.js
+++ 04-Web-APIs/01-Activities/16-Stu_Keyboard-Events/Unsolved/assets/js/script.js
@@ -1,5 +1,12 @@
+var keyPressedEl = document.querySelector("#key");
+var keyCodeEl = document.querySelector("#code");
+var StatusEl = document.querySelector("#status");
+
 function keydownAction(event) {
   // TODO: Complete keydown function
+  keyPressedEl.textContent = event.key;
+  keyCodeEl.textContent = event.code;
+  StatusEl.textContent = event.type;
 }
 
 function keyupAction() {
@@ -8,4 +15,4 @@ function keyupAction() {
 
 document.addEventListener("keyup", keyupAction);
 // TODO: Add Event Listener for "keydown" event
-
+document.addEventListener("keydown", keydownAction);
