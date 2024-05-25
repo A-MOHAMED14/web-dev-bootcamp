00046d1bb55b5c8a9e64edfa567fcfd4f89474e1
A-MOHAMED14 <amin800@hotmail.co.uk>
2024-05-25 13:05:45 +0100

    Use data attributes to display hidden number

--- 04-Web-APIs/01-Activities/20-Stu_Data-Attributes/Unsolved/assets/js/script.js
+++ 04-Web-APIs/01-Activities/20-Stu_Data-Attributes/Unsolved/assets/js/script.js
@@ -1,7 +1,22 @@
 var container = document.querySelector(".container");
 
-container.addEventListener("click", function(event) {
+container.addEventListener("click", function (event) {
   var element = event.target;
 
   // TODO: Complete function
+  if (element.matches(".box")) {
+    var state = element.getAttribute("data-state");
+
+    if (state === "hidden") {
+      element.setAttribute("data-state", "visible");
+      element.dataset.state = "visible";
+
+      element.textContent = element.dataset.number;
+    } else {
+      element.setAttribute("data-state", "hidden");
+      element.dataset.state = "hidden";
+
+      element.textContent = "";
+    }
+  }
 });
