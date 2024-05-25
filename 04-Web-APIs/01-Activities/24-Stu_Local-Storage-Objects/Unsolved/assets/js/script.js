057ec4cc6e3b0e075ca417e2f0fd07a2c66f7146
A-MOHAMED14 <amin800@hotmail.co.uk>
2024-05-25 14:12:55 +0100

    Save object using local storage

--- 04-Web-APIs/01-Activities/24-Stu_Local-Storage-Objects/Unsolved/assets/js/script.js
+++ 04-Web-APIs/01-Activities/24-Stu_Local-Storage-Objects/Unsolved/assets/js/script.js
@@ -4,11 +4,20 @@ var emailInput = document.querySelector("#email");
 var passwordInput = document.querySelector("#password");
 var signUpButton = document.querySelector("#sign-up");
 
-signUpButton.addEventListener("click", function(event) {
+signUpButton.addEventListener("click", function (event) {
   event.preventDefault();
-  
-  // TODO: Create user object from submission
 
-  // TODO: Set new submission to local storage 
-  
+  // TODO: Create user object from submission
+  const userData = {
+    firstName: firstNameInput.value,
+    lastName: lastNameInput.value,
+    email: emailInput.value,
+    password: passwordInput.value,
+  };
+  console.log(userData, "<-------");
+
+  // TODO: Set new submission to local storage
+  const userDataStr = JSON.stringify(userData);
+  localStorage.setItem("user", userDataStr);
+  console.log(localStorage.getItem("user"), "<<<<<<<<<");
 });
