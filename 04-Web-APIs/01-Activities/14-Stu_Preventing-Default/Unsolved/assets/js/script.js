f777e8924eee7b57e3862b7d3e49d787c40d7794
A-MOHAMED14 <amin800@hotmail.co.uk>
2024-05-25 11:11:41 +0100

    Prevent default behaviour of forms to calculate tip amount

--- 04-Web-APIs/01-Activities/14-Stu_Preventing-Default/Unsolved/assets/js/script.js
+++ 04-Web-APIs/01-Activities/14-Stu_Preventing-Default/Unsolved/assets/js/script.js
@@ -1,6 +1,7 @@
 var tipEl = document.querySelector("#tip-percentage");
 var totalEl = document.querySelector("#total");
 var submitEl = document.querySelector("#submit");
+var formEl = document.querySelector("#form");
 
 function calculateTip(total, tipPercentage) {
   var roundedResult = (total * tipPercentage).toFixed(2);
@@ -12,12 +13,16 @@ function calculateTotal(total, tipAmount) {
 }
 
 function addTip(event) {
-  var tipPercentage = tipEl.value * .01;
+  var tipPercentage = tipEl.value * 0.01;
   var total = totalEl.value;
   var tipAmount = calculateTip(total, tipPercentage);
   var newTotal = calculateTotal(tipAmount, total);
   document.querySelector("#tip-amount").textContent = tipAmount;
   document.querySelector("#new-total").textContent = newTotal.toFixed(2);
 }
+
+formEl.addEventListener("submit", (event) => {
+  event.preventDefault();
+});
 
 submitEl.addEventListener("click", addTip);
