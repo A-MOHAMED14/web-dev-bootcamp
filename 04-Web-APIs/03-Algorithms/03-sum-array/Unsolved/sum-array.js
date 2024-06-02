c82555c4c58a7df3c79794705f058d51dec4b344
A-MOHAMED14 <amin800@hotmail.co.uk>
2024-06-02 12:04:41 +0100

    Create a function that adds up all the numbers in a given array and returns the total.

--- 04-Web-APIs/03-Algorithms/03-sum-array/Unsolved/sum-array.js
+++ 04-Web-APIs/03-Algorithms/03-sum-array/Unsolved/sum-array.js
@@ -1,3 +1,11 @@
 // Write code to add all the numbers in `arr` and return the total
 
-var sumArray = function(arr) {};
+const sumArray = function (arr) {
+  let total = 0;
+
+  arr.forEach((number) => {
+    total += number;
+  });
+
+  return total;
+};
