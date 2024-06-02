7e43aee4d51cf99b90bd55d4e9fa03eba81e0bed
A-MOHAMED14 <amin800@hotmail.co.uk>
2024-06-02 12:56:26 +0100

    Create a function that counts the number of vowels in a given string.

--- 05-Third-Party-APIs/03-Algorithms/03-vowel-count/Unsolved/vowel-count.js
+++ 05-Third-Party-APIs/03-Algorithms/03-vowel-count/Unsolved/vowel-count.js
@@ -1,3 +1,24 @@
 // Write code to return the the number of vowels in `str`
 
-var vowelCount = function(str) {};
+const vowelCount = function (str) {
+  let vowelsTotal = 0;
+
+  const lowerCaseStr = str.toLowerCase();
+
+  for (let i = 0; i < lowerCaseStr.length; i++) {
+    const letter = lowerCaseStr[i];
+    if (
+      letter === "a" ||
+      letter === "e" ||
+      letter === "i" ||
+      letter === "o" ||
+      letter === "u"
+    ) {
+      vowelsTotal += 1;
+    } else {
+      vowelsTotal;
+    }
+  }
+
+  return vowelsTotal;
+};
