8147810a77ab2ca1ceb952f6ed82c5981951fc22
A-MOHAMED14 <amin800@hotmail.co.uk>
2024-06-02 12:22:49 +0100

    Create a function that returns the largest number present in a given array.

--- 05-Third-Party-APIs/03-Algorithms/02-max-num/Unsolved/max-num.js
+++ 05-Third-Party-APIs/03-Algorithms/02-max-num/Unsolved/max-num.js
@@ -1,3 +1,13 @@
 // Write code to return the largest number in the given array
 
-var maxNum = function(arr) {};
+const maxNum = function (arr) {
+  let largestNum = 0;
+
+  arr.forEach((number) => {
+    if (number > largestNum) {
+      largestNum = number;
+    }
+  });
+
+  return largestNum;
+};
