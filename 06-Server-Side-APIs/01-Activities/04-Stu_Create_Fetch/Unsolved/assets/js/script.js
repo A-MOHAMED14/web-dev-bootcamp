0a0cedbd18ed7d19387e5d28c72a70cca777cb99
A-MOHAMED14 <amin800@hotmail.co.uk>
2024-06-03 12:55:03 +0100

    Implement a fetch() Request to Display User Repositories

--- 06-Server-Side-APIs/01-Activities/04-Stu_Create_Fetch/Unsolved/assets/js/script.js
+++ 06-Server-Side-APIs/01-Activities/04-Stu_Create_Fetch/Unsolved/assets/js/script.js
@@ -1,21 +1,22 @@
-var repoList = document.querySelector('ul');
-var fetchButton = document.getElementById('fetch-button');
+var repoList = document.querySelector("ul");
+var fetchButton = document.getElementById("fetch-button");
 
 //getApi function is called when the fetchButton is clicked
 
 function getApi() {
   // Insert the API url to get a list of your repos
-  var requestUrl = '';
+  var requestUrl = "https://api.github.com/users/A-MOHAMED14/repos";
 
   fetch(requestUrl)
     .then(function (response) {
       return response.json();
     })
     .then(function (data) {
+      console.log(data, "<----");
       //looping over the fetch response and inserting the URL of your repos into a list
       for (var i = 0; i < data.length; i++) {
         //Create a list element
-        var listItem = document.createElement('li');
+        var listItem = document.createElement("li");
 
         //Set the text of the list element to the JSON response's .html_url property
         listItem.textContent = data[i].html_url;
@@ -26,4 +27,4 @@ function getApi() {
     });
 }
 
-fetchButton.addEventListener('click', getApi);
+fetchButton.addEventListener("click", getApi);
