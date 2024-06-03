2551c0a2815a97446f3f90207213d51dd7335797
A-MOHAMED14 <amin800@hotmail.co.uk>
2024-06-03 16:00:52 +0100

    Implement Status Checking to Display a 404 Message

--- 06-Server-Side-APIs/01-Activities/14-Stu_Fetch_Status/Unsolved/assets/js/script.js
+++ 06-Server-Side-APIs/01-Activities/14-Stu_Fetch_Status/Unsolved/assets/js/script.js
@@ -1,6 +1,6 @@
-var badRequestUrl = 'https://api.github.com/orgs/nodejs/oreps?per_page=5';
+var badRequestUrl = "https://api.github.com/orgs/nodejs/oreps?per_page=5";
 
-var responseText = document.getElementById('response-text');
+var responseText = document.getElementById("response-text");
 
 function getApi(requestUrl) {
   fetch(requestUrl)
@@ -8,7 +8,11 @@ function getApi(requestUrl) {
       // Check the console first to see the response.status
       console.log(response.status);
       // Then write the conditional based on that response.status value
-      // Make sure to display the response on the page
+      if (response.status !== 200) {
+        // Make sure to display the response on the page
+        responseText.textContent = response.status;
+      }
+      return response.json();
     })
     .then(function (data) {
       console.log(data);
