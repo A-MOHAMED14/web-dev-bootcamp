443e864908cce47c7a69298c78ed444505d578bb
A-MOHAMED14 <amin800@hotmail.co.uk>
2024-06-03 17:01:41 +0100

    Add Comments to Implementation of API Parameters

--- 06-Server-Side-APIs/01-Activities/16-Stu_Deconstruct_Parameters/Unsolved/assets/js/script.js
+++ 06-Server-Side-APIs/01-Activities/16-Stu_Deconstruct_Parameters/Unsolved/assets/js/script.js
@@ -9,3 +9,7 @@ fetch(
     console.log(data);
   });
 // Parameter explanation.
+// per_page=10: this parameter limits the number of results per page to 10
+// state=open: this parameter represents the current state of the issue, in this case is it open.
+// sort=created: sorts the results based on the created property
+// direction=desc: sorts the results from newest to oldest 
