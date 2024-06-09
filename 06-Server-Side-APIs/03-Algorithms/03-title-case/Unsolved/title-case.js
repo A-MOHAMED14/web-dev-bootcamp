4bfb38072ac6e50b7f9046126e19df7a3f1f8c06
A-MOHAMED14 <amin800@hotmail.co.uk>
2024-06-09 12:17:59 +0100

    Create a function that capitalizes the first letter of every word in a given string.

--- 06-Server-Side-APIs/03-Algorithms/03-title-case/Unsolved/title-case.js
+++ 06-Server-Side-APIs/03-Algorithms/03-title-case/Unsolved/title-case.js
@@ -1,3 +1,15 @@
 // Write code to create a function takes a string and returns a new string with the first letters of each word capitalized
 
-var titleCase = function(str) {};
+var titleCase = function (str) {
+  const wordsArr = str.split(" ");
+  let strArr = [];
+
+  for (let i = 0; i < wordsArr.length; i++) {
+    strArr.push(
+      wordsArr[i][0].toUpperCase() + wordsArr[i].slice(1, wordsArr[i].length)
+    );
+  }
+  const capitalizedStr = strArr.join(" ");
+
+  return capitalizedStr;
+};
