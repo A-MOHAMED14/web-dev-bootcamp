9abf30a0f15e4447bc3a0db97369586068601d8f
A-MOHAMED14 <amin800@hotmail.co.uk>
2024-06-09 12:01:39 +0100

    Create a function that returns the factorial of a given number.

--- 06-Server-Side-APIs/03-Algorithms/02-factorial/Unsolved/factorial.js
+++ 06-Server-Side-APIs/03-Algorithms/02-factorial/Unsolved/factorial.js
@@ -1,3 +1,11 @@
 // Write code to create a function that returns the factorial of `num`
 
-var factorial = function(num) {};
+var factorial = function (num) {
+  let factorial = 1;
+
+  for (let i = 1; i <= num; i++) {
+    factorial *= i;
+  }
+
+  return factorial;
+};
