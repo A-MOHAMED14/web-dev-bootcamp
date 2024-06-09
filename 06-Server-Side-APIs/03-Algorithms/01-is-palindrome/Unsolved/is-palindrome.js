175c5ada68cdbbaf3bba6cdbfb79581f463f3057
A-MOHAMED14 <amin800@hotmail.co.uk>
2024-06-09 11:52:10 +0100

    create a function that takes in a string and determines whether or not it is a palindrome

--- 06-Server-Side-APIs/03-Algorithms/01-is-palindrome/Unsolved/is-palindrome.js
+++ 06-Server-Side-APIs/03-Algorithms/01-is-palindrome/Unsolved/is-palindrome.js
@@ -1,3 +1,21 @@
 // Write code that returns true if `str` is a palindrome, and false if `str` is not a palindrome
 
-var isPalindrome = function(str) {};
+var isPalindrome = function (str) {
+  const letters = str.split("");
+
+  let reversedLettersArr = [];
+
+  for (let i = letters.length - 1; i >= 0; i--) {
+    reversedLettersArr.push(letters[i]);
+  }
+
+  const reversedWord = reversedLettersArr.join("");
+
+  console.log(reversedWord);
+
+  if (reversedWord === str) {
+    return true;
+  } else {
+    return false;
+  }
+};
