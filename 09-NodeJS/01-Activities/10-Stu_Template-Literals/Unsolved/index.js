2cd7b75dc876534dd45b2d7eb2dc050bf80f6ea2
A-MOHAMED14 <amin800@hotmail.co.uk>
2024-06-25 12:40:56 +0100

    Implement Template Literals

--- 09-NodeJS/01-Activities/10-Stu_Template-Literals/Unsolved/index.js
+++ 09-NodeJS/01-Activities/10-Stu_Template-Literals/Unsolved/index.js
@@ -1,11 +1,11 @@
-// music should be an object with title, artist, and album properties
-const music = {
-  // code here
- 
+// book should be an object with title, author, and date published properties
+const book = {
+  title: "Harry Potter and the Philosopher's Stone",
+  author: "J.K. Rowling",
+  published: "26 June 1997",
 };
 
-// Write code between the backticks tags to output the data from the music object above.
-const songSnippet = ``;
+// Write code between the backticks tags to output the data from the book object above.
+const bookSnippet = `The first Harry Potter book ${book.author} wrote was ${book.title} and was originally published on ${book.published}`;
 
-console.log(songSnippet)
-
+console.log(bookSnippet);
