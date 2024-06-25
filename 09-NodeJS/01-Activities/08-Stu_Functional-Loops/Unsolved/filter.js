28986fc4fabf201f9f784f7cca903871008e0696
A-MOHAMED14 <amin800@hotmail.co.uk>
2024-06-25 11:27:20 +0100

    Add Comments to Implementation of Functional Loops

--- 09-NodeJS/01-Activities/08-Stu_Functional-Loops/Unsolved/filter.js
+++ 09-NodeJS/01-Activities/08-Stu_Functional-Loops/Unsolved/filter.js
@@ -1,8 +1,8 @@
 const originalArray = [1, 3, 2, 5, 10];
 
-// evenNumbers is equal to a new array of even numbers returned by filter(). filter() will use an anonymous function as a condition. 
-const evenNumbers = originalArray.filter(function(data) {
-  // The anonymous function uses a conditional to check if a number is divisible by 2. 
+// evenNumbers is equal to a new array of even numbers returned by filter(). filter() will use an anonymous function as a condition.
+const evenNumbers = originalArray.filter(function (data) {
+  // The anonymous function uses a conditional to check if a number is divisible by 2.
   if (data % 2 === 0) {
     // If a number is even (divisible by 2), the condition returns true.
     return true;
@@ -12,7 +12,7 @@ const evenNumbers = originalArray.filter(function(data) {
 console.log(evenNumbers);
 console.log(originalArray);
 
-const isPrime = num => {
+const isPrime = (num) => {
   // Set up a loop the starts with 2 and continues to increment i as long as i is less than 2
   for (let i = 2; i < num; i++) {
     // isPrime will return false if num is divisible by any number other than 1 or num.
@@ -22,11 +22,16 @@ const isPrime = num => {
   return num !== 1;
 };
 
-
-// TODO: Describe how filter is working in this example. What will the value of primeArray be? 
+// TODO: Describe how filter is working in this example. What will the value of primeArray be?
 const primeArray = originalArray.filter(isPrime);
+// primeArray is equal to a new array returned by filter().
+// filter() invokes the isPrime function which checks if the numbers in the originalArray is a prime number
+// if a number is prime, it will return that number to the new array, primeArray
+// [3,2,5]
 
-
-// TODO: Describe how filter is working in this example. What will the value of moreThan5Array be? 
-const moreThan5Array = originalArray.filter(num => num > 5);
-
+// TODO: Describe how filter is working in this example. What will the value of moreThan5Array be?
+const moreThan5Array = originalArray.filter((num) => num > 5);
+// moreThan5Array is equal to a new array returned by filter().
+// a filter runs through the originalArray.
+// if a number is greater than 5, it will return that number to the new array, moreThan5Array
+// [10]
--- 09-NodeJS/01-Activities/08-Stu_Functional-Loops/Unsolved/map.js
+++ 09-NodeJS/01-Activities/08-Stu_Functional-Loops/Unsolved/map.js
@@ -1,7 +1,7 @@
 const originalArray = [1, 3, 2, 5, 10];
 
-// doubledArray is equal to a new array of numbers multiplied by 2 and returned by map(). map() will use an anonymous function as a condition. 
-const doubledArray = originalArray.map(function(data) {
+// doubledArray is equal to a new array of numbers multiplied by 2 and returned by map(). map() will use an anonymous function as a condition.
+const doubledArray = originalArray.map(function (data) {
   // The anonymous function returns each number multiplied by 2. The returned result is added to a new array.
   return data * 2;
 });
@@ -9,15 +9,21 @@ const doubledArray = originalArray.map(function(data) {
 console.log(doubledArray);
 console.log(originalArray);
 
-
 // TODO: Describe how map is working in the example below. What will the value of tripledArray be?
-const tripledArray = originalArray.map(data => data * 3);
+const tripledArray = originalArray.map((data) => data * 3);
+// A map loops through the original array, and creates a new array with same length as the originalArray, but with each array item multiplied by 3
+// [3, 9, 6, 15, 30]
 
 // TODO: Describe how map is working in the example below. What will the value of oddOrEven be?
-const oddOrEven = originalArray.map(num => {
+const oddOrEven = originalArray.map((num) => {
   if (num % 2 === 0) {
-    return 'even'
+    return "even";
   } else {
-    return 'odd'
+    return "odd";
   }
 });
+
+// A map loops through the original array, and creates a new array with same length as the originalArray
+// each array item goes through a check to determine whether it is even or odd
+// if even, the string 'even' will be returned in the new array in place of the number, vice versa for odd numbers
+// ["odd", "odd", "even", "odd", "even"]
