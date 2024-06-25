68f85e62189fb4d8b2c47bd1b918543cbfdad2df
A-MOHAMED14 <amin800@hotmail.co.uk>
2024-06-25 13:36:56 +0100

    Add Comments to Implementation of fs.appendFile()

--- 09-NodeJS/01-Activities/14-Stu_appendFile/Unsolved/index.js
+++ 09-NodeJS/01-Activities/14-Stu_appendFile/Unsolved/index.js
@@ -1,8 +1,18 @@
 // TODO: What are we importing?
-const fs = require('fs');
+// The fs module to allow interaction with the file system
+const fs = require("fs");
 
 // TODO: Add comments to explain each of the three arguments of appendFile()
-fs.appendFile('log.txt', `${process.argv[2]}\n`, (err) =>
+// first parameter is the file path to which text will be appened to
+// second parameter is the data which will be the text that will be appened to the log.txt file
+// third parameter is a callback function
+
+fs.appendFile("log.txt", `${process.argv[2]}\n`, (err) =>
   // TODO: Describe how this ternary operator works
-  err ? console.error(err) : console.log('Commit logged!')
+  // The ternary operator is a simplified conditional operator like if / else.
+  // It takes a condition which will be evaluated, followed by a ?
+  // Then an expression which will be executed if truthy, followed by a :
+  // Then an expression which will be executed if falsy
+
+  err ? console.error(err) : console.log("Commit logged!")
 );
