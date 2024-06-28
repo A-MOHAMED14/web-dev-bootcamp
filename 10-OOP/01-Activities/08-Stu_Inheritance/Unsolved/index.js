24d41fd5d0d8d32a1ed3f0986ec2c43702d8bf41
A-MOHAMED14 <amin800@hotmail.co.uk>
2024-06-28 18:24:47 +0100

    Implement Inheritance for Multiple Classes

--- 10-OOP/01-Activities/08-Stu_Inheritance/Unsolved/index.js
+++ 10-OOP/01-Activities/08-Stu_Inheritance/Unsolved/index.js
@@ -1,12 +1,18 @@
 // TODO: Create a class constructor named ForumItem that takes in 'authorName' and 'createdOn'.
+class ForumItem {
+  constructor(authorName, createdOn) {
+    this.authorName = authorName;
+    this.createdOn = createdOn;
+  }
+}
 
 // TODO: Setup BlogPost and Comment so they inherit their structure from ForumItem.
+
 class BlogPost {
   constructor(authorName, title, text, createdOn) {
-    this.authorName = authorName;
+    super(authorName, createdOn);
     this.title = title;
     this.text = text;
-    this.createdOn = createdOn;
     this.comments = [];
   }
 
@@ -15,11 +21,10 @@ class BlogPost {
   }
 }
 
-class Comment {
+class Comment extends ForumItem {
   constructor(authorName, text, createdOn) {
-    this.authorName = authorName;
+    super(authorName, createdOn);
     this.text = text;
-    this.createdOn = createdOn;
   }
 }
 
@@ -28,3 +33,19 @@ class Comment {
 // TODO: Create a new object using the BlogPost class constructor.
 
 // TODO: Log both newly created BlogPost and Comment to the console.
+
+const blogPost1 = new BlogPost(
+  "John Wick",
+  "15/05/2019",
+  "Parabellum",
+  "Hello from the Continental!"
+);
+
+const comment1 = new Comment(
+  "Franklin Saint",
+  "08/08/2018",
+  "This is the best post in the world!"
+);
+
+console.log(blogPost1);
+console.log(comment1);
