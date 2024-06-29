aae3b0f7c862e84e818c6bcebac89d58cba852b7
A-MOHAMED14 <amin800@hotmail.co.uk>
2024-06-29 15:12:15 +0100

    Implement and Test for an Exception

--- 10-OOP/01-Activities/18-Stu_Exception-Tests/Unsolved/test/blogPost.test.js
+++ 10-OOP/01-Activities/18-Stu_Exception-Tests/Unsolved/test/blogPost.test.js
@@ -1,6 +1,15 @@
-const BlogPost = require('../blogPost.js');
+const BlogPost = require("../blogPost.js");
 
-describe('BlogPost', () => {
+describe("BlogPost", () => {
   // TODO: Write a test that verifies that BlogPost throws an error when the authorName provided is less than 2 characters long.
+  describe("authorName length", () => {
+    it.only("Should throw an error if author name is lest than 2 characters", () => {
+      const expected = "Author must be at least 2 characters long.";
+      const newPost = new BlogPost("Ab");
+      const actual = "Author must be at least 2 characters long.";
+
+      expect(actual).toEqual(expected);
+    });
+  });
   // TODO: Write a test that verifies that BlogPost throws an error when the authorName provided does not only contain uppercase, lowercase, numbers, dashes, and underscores.
 });
