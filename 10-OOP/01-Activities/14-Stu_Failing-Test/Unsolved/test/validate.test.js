cb367491b658940a22415ada681c29a38b09b92e
A-MOHAMED14 <amin800@hotmail.co.uk>
2024-06-29 13:07:51 +0100

    Write a Failing Test

--- 10-OOP/01-Activities/14-Stu_Failing-Test/Unsolved/test/validate.test.js
+++ 10-OOP/01-Activities/14-Stu_Failing-Test/Unsolved/test/validate.test.js
@@ -1,5 +1,16 @@
 // TODO: Import the Validate class.
+const Validate = require("../validate.js");
 
 // TODO: Describe a testing suite for checking the functionality of the Validate class.
+describe("Validate", () => {
+  describe("isPassword", () => {
+    it("Should return false for an empty password", () => {
+      const expected = false;
+      const passwordCheck = new Validate();
+      const actual = passwordCheck.isPassword("");
+      expect(actual).toEqual(expected);
+    });
+  });
+});
 
 // TODO: Write a test that asserts that isPassword() returns false when passed an empty string.
