17532986e511c0c772d3998993d0dcc59d31e162
A-MOHAMED14 <amin800@hotmail.co.uk>
2024-07-03 16:09:35 +0100

    Implement Logging for GET and POST Requests

--- 11-Express/01-Activities/14-Stu_POST-Requests/Unsolved/server.js
+++ 11-Express/01-Activities/14-Stu_POST-Requests/Unsolved/server.js
@@ -1,24 +1,32 @@
-const express = require('express');
+const express = require("express");
 
 const PORT = 3001;
 
 const app = express();
 
 // TODO: Create a GET method for `/api/reviews` that logs when a user's request has been received
-app.get('/api/reviews', (req, res) => {
-  // Your code here
+app.get("/api/reviews", (req, res) => {
+  console.info(`${req.method} request recieved`);
+  res.json(`${req.method} request recieved`);
 });
 
 // TODO: Create a POST request for `/api/reviews` that logs when a user's request has been received
-// Your code here
+app.post("/api/reviews", (req, res) => {
+  console.info(`${req.method} request recieved`);
+  res.json(`${req.method} request recieved`);
+});
 
 // TODO: Create a GET request for `api/upvotes` that logs when a user's request has been received
-app.get('/api/upvotes', (req, res) => {
-  // Your code here
+app.get("/api/upvotes", (req, res) => {
+  console.info(`${req.method} request recieved`);
+  res.json(`${req.method} request recieved`);
 });
 
 // TODO: Create a POST request for `api/upvotes` that logs when a user's request has been received
-// Your code here
+app.post("/api/upvotes", (req, res) => {
+  console.info(`${req.method} request recieved`);
+  res.json(`${req.method} request recieved`);
+});
 
 app.listen(PORT, () =>
   console.log(`Express server listening on port ${PORT}!`)
