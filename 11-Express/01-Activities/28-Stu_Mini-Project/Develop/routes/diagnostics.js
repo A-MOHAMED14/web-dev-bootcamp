cc6a3826ccfa88926648b74c2bdaabe3ffaec1d3
A-MOHAMED14 <amin800@hotmail.co.uk>
2024-07-04 19:54:21 +0100

    Create a POST route for /api/diagnostics

--- 11-Express/01-Activities/28-Stu_Mini-Project/Develop/routes/diagnostics.js
+++ 11-Express/01-Activities/28-Stu_Mini-Project/Develop/routes/diagnostics.js
@@ -14,6 +14,23 @@ diagnostics.get("/", (req, res) => {
 // POST Route for a error logging
 diagnostics.post("/", (req, res) => {
   // TODO: Logic for appending data to the db/diagnostics.json file
+  const { isValid, errors } = req.body;
+
+  const payload = {
+    time: Date.now(),
+    error_id: uuidv4(),
+    errors,
+  };
+
+  if (!isValid) {
+    readAndAppend(payload, "../db/diagnostics.json");
+    res.json(`Diagnostic information added 🔧`);
+  } else {
+    res.json({
+      message: "Object is valid, not logging. Check front end implementation",
+      error_id: payload.error_id,
+    });
+  }
 });
 
 module.exports = diagnostics;
