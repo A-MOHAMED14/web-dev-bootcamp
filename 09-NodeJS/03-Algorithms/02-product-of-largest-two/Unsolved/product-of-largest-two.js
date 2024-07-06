acdc0cd4e685253191f1f3fbc66c3920224ae945
A-MOHAMED14 <amin800@hotmail.co.uk>
2024-07-06 11:29:44 +0100

    Create a function that finds the largest two numbers in an array and returns the product of both numbers.

--- 09-NodeJS/03-Algorithms/02-product-of-largest-two/Unsolved/product-of-largest-two.js
+++ 09-NodeJS/03-Algorithms/02-product-of-largest-two/Unsolved/product-of-largest-two.js
@@ -1,3 +1,14 @@
 // Write code to create a function that accepts an array of numbers, finds the largest two numbers, and returns the product of the two
 
-var productOfLargestTwo = function(arr) {};
+var productOfLargestTwo = function (arr) {
+  const ascArr = arr.sort((a, b) => a - b);
+
+  for (let i = 0; i < ascArr.length; i++) {
+    const largest = ascArr[ascArr.length - 1];
+    const secondLargest = ascArr[ascArr.length - 2];
+
+    const product = largest * secondLargest;
+    console.log(product);
+    return product;
+  }
+};
