acd1f09f475ad96821c574bac93e1cad83e014c1
A-MOHAMED14 <amin800@hotmail.co.uk>
2024-07-06 12:12:49 +0100

    Create a function that takes a string and returns the string in camel case.

--- 09-NodeJS/03-Algorithms/03-camel-case/Unsolved/camel-case.js
+++ 09-NodeJS/03-Algorithms/03-camel-case/Unsolved/camel-case.js
@@ -1,3 +1,24 @@
 // Write code to create a function that accepts a string and returns the string in camelCase
 
-var camelCase = function(str) {};
+var camelCase = function (str) {
+  const wordsArr = str.split(" ");
+
+  let camelCaseStr = "";
+
+  for (let i = 0; i < wordsArr.length; i++) {
+    const currentWord = wordsArr[i];
+
+    if (i === 0) {
+      const firstWord = currentWord.toLowerCase();
+      camelCaseStr += firstWord;
+    } else {
+      const capFirstLetter =
+        currentWord.charAt(0).toUpperCase() +
+        currentWord.slice(1).toLowerCase();
+
+      camelCaseStr += capFirstLetter;
+    }
+  }
+
+  return camelCaseStr;
+};
