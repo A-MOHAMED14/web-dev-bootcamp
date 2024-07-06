79b5dfa84c2bec2cad6d7b42ee010f11cbc0adca
A-MOHAMED14 <amin800@hotmail.co.uk>
2024-07-06 10:36:49 +0100

    Create a function that takes a string and returns an object containing the characters in the string and the number of times they appear.

--- 09-NodeJS/03-Algorithms/01-character-count/Unsolved/character-count.js
+++ 09-NodeJS/03-Algorithms/01-character-count/Unsolved/character-count.js
@@ -1,3 +1,17 @@
 // Write code to create a function that accepts a string and returns an object containing the number of times each character appears in the string
 
-var characterCount = function(str) {};
+var characterCount = function (str) {
+  let characterCount = {};
+
+  for (let i = 0; i < str.length; i++) {
+    const char = str[i];
+
+    if (char in characterCount) {
+      characterCount[char]++;
+    } else {
+      characterCount[char] = 1;
+    }
+  }
+
+  return characterCount;
+};
