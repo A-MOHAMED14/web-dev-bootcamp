d8e8ed9630d8e69c40ff60506b3f59fdea8061da
A-MOHAMED14 <amin800@hotmail.co.uk>
2024-07-07 11:09:00 +0100

    add function to calculate max songs fitting into 60-minute playlist

--- 11-Express/03-Algorithms/01-most-songs-in-playlist/Unsolved/most-songs-in-playlist.js
+++ 11-Express/03-Algorithms/01-most-songs-in-playlist/Unsolved/most-songs-in-playlist.js
@@ -2,6 +2,23 @@
 // Each number represents the duration of a song in minutes
 // Return the maximum number of songs from the list that can fit in a 60 minute playlist
 
-var mostSongsInPlaylist = function(arr) {
-  // add your code here...
+var mostSongsInPlaylist = function (arr) {
+  const ascArr = arr.sort((a, b) => a - b);
+
+  const maxDuration = 60;
+  let sum = 0;
+  let numOfSongs = 0;
+
+  for (let i = 0; i < ascArr.length; i++) {
+    const currentDuration = ascArr[i];
+
+    if (sum + currentDuration <= maxDuration) {
+      sum += currentDuration;
+      numOfSongs++;
+    } else {
+      break;
+    }
+  }
+
+  return numOfSongs;
 };
