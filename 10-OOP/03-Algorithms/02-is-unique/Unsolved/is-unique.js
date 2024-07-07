7c6883167594b5ec5596d85585929c845039dd78
A-MOHAMED14 <amin800@hotmail.co.uk>
2024-07-07 10:01:47 +0100

    Add function to check for unique numbers in an array

--- 10-OOP/03-Algorithms/02-is-unique/Unsolved/is-unique.js
+++ 10-OOP/03-Algorithms/02-is-unique/Unsolved/is-unique.js
@@ -1,4 +1,17 @@
 // Write code to create a function that accepts an array numbers
 // Return `true` is no number appears in the array more than once, else return `false`
 
-var isUnique = function(arr) {};
+var isUnique = function (arr) {
+  console.log(arr);
+
+  for (let i = 0; i < arr.length; i++) {
+    const firstNum = arr[0];
+    const currentNum = arr[i + 1];
+
+    if (firstNum === currentNum) {
+      return false;
+    }
+  }
+
+  return true;
+};
