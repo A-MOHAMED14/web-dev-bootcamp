77d68048843758d662c13605c4ae17cd0fbd0365
A-MOHAMED14 <amin800@hotmail.co.uk>
2024-07-07 12:01:05 +0100

    add function to count occurrences of 'X' in a nested array

--- 11-Express/03-Algorithms/03-array-search-2d/Unsolved/array-search-2d.js
+++ 11-Express/03-Algorithms/03-array-search-2d/Unsolved/array-search-2d.js
@@ -2,4 +2,17 @@
 // Each 2D element contains either the string "X" or "O" (both capitalized)
 // Return the number of times "X" appears in the 2D array
 
-var arraySearch2D = function(arr) {};
+var arraySearch2D = function (arr) {
+  let xCount = 0;
+
+  for (let i = 0; i < arr.length; i++) {
+    currentArr = arr[i];
+
+    currentArr.forEach((arrItem) => {
+      if (arrItem === "X") {
+        xCount++;
+      }
+    });
+  }
+  return xCount;
+};
