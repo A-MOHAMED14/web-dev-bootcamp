07a5dfd24f44fe5dbab35626d5e2213039b1e5e9
A-MOHAMED14 <amin800@hotmail.co.uk>
2024-07-07 10:36:57 +0100

    Add function to check if two strings are anagrams

--- 10-OOP/03-Algorithms/03-is-anagram/Unsolved/is-anagram.js
+++ 10-OOP/03-Algorithms/03-is-anagram/Unsolved/is-anagram.js
@@ -1,4 +1,21 @@
 // Write code to create a function that accepts two strings
 // Return true if the strings are anagrams of each other, otherwise return false
 
-var isAnagram = function(strA, strB) {};
+var isAnagram = function (strA, strB) {
+  const stringA = strA.toLowerCase();
+  const stringB = strB.toLowerCase();
+
+  if (stringA.length === stringB.length) {
+    const arrA = stringA.split("");
+    const arrB = stringB.split("");
+
+    const str1 = arrA.sort().join("");
+    const str2 = arrB.sort().join("");
+
+    if (str1 === str2) {
+      return true;
+    }
+  }
+
+  return false;
+};
