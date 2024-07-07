9a7e8ae24cf7e16bfdcd4d9f60cb83056e085184
A-MOHAMED14 <amin800@hotmail.co.uk>
2024-07-07 11:41:57 +0100

    add function to check if a number is a perfect square

--- 11-Express/03-Algorithms/02-is-perfect-square/Unsolved/is-perfect-square.js
+++ 11-Express/03-Algorithms/02-is-perfect-square/Unsolved/is-perfect-square.js
@@ -1,4 +1,13 @@
 // Write code to create a function that accepts a number and returns true if the number is a perfect square, otherwise it returns false.
 // You may not use the built-in Math.sqrt method
 
-var isPerfectSquare = function(num) {};
+var isPerfectSquare = function (num) {
+  const sqrt = num ** 0.5;
+  const fraction = sqrt - Math.floor(num ** 0.5);
+
+  if (num >= 0 && fraction === 0) {
+    return true;
+  }
+
+  return false;
+};
