724024c6f63195d23ae4fd12e7432e8164a01f58
A-MOHAMED14 <amin800@hotmail.co.uk>
2024-07-08 18:54:59 +0100

    Use Primary Key and Foreign Key to Define Relationship Between Tables

--- 12-SQL/01-Activities/16-Stu_Foreign-Primary-Key/Unsolved/db/schema.sql
+++ 12-SQL/01-Activities/16-Stu_Foreign-Primary-Key/Unsolved/db/schema.sql
@@ -1,1 +1,17 @@
 -- Write your Schema Here -- 
+DROP DATABASE IF EXISTS grocery_db;
+CREATE TABLE grocery_db;
+
+\c grocery_db;
+
+CREATE TABLE customers (
+    id SERIAL PRIMARY KEY,
+    first_name VARCHAR(30),
+    last_name VARCHAR(30),
+);
+
+CREATE TABLE customer_orders (
+    id SERIAL PRIMARY KEY,
+    customer_id INTEGER FOREIGN KEY REFERENCES customers(id),
+    order_details TEXT
+);
