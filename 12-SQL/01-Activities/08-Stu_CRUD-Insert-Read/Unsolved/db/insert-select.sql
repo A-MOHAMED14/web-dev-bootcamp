3ec1ea375042450c3729a7f2ca61842e88e958ac
A-MOHAMED14 <amin800@hotmail.co.uk>
2024-07-08 16:32:49 +0100

    Add Comments to Implementation of Selecting and Inserting Data Using PostgreSQL

--- 12-SQL/01-Activities/08-Stu_CRUD-Insert-Read/Unsolved/db/insert-select.sql
+++ 12-SQL/01-Activities/08-Stu_CRUD-Insert-Read/Unsolved/db/insert-select.sql
@@ -1,8 +1,9 @@
 -- TODO: What is the SQL code below doing?
+-- Tells psql to insert data into id & name columns respectively  
 INSERT INTO biographies (id, name)
 VALUES
     ( 001, 'Diary of Anne Frank'),
     ( 002, 'Frida: A Biography of Frida Kahlo'),
     ( 003, 'Long Walk to Freedom');
-
+-- Inserts multiple biographies
 SELECT * FROM biographies;
