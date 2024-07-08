c866609c814c48579a6b19673303082ae0d70547
A-MOHAMED14 <amin800@hotmail.co.uk>
2024-07-08 16:02:58 +0100

    Add table to database

--- 12-SQL/01-Activities/06-Stu_Tables/Unsolved/db/schema.sql
+++ 12-SQL/01-Activities/06-Stu_Tables/Unsolved/db/schema.sql
@@ -4,3 +4,10 @@ CREATE DATABASE books_db;
 \c books_db;
 
 -- Add biography table --
+DROP DATABASE IF EXISTS biography_db;
+CREATE DATABASE biography_db;
+
+CREATE TABLE biographies (
+    id INTEGER,
+    name VARCHAR(100)
+);
