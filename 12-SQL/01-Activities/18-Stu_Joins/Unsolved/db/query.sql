c4867bb4679f4fa015f824ee44f6a0b659f79621
A-MOHAMED14 <amin800@hotmail.co.uk>
2024-07-08 18:55:24 +0100

    Implement Join Clause to Combine Two Tables

--- 12-SQL/01-Activities/18-Stu_Joins/Unsolved/db/query.sql
+++ 12-SQL/01-Activities/18-Stu_Joins/Unsolved/db/query.sql
@@ -1,1 +1,5 @@
 -- Add your code below and execute file in the PostgreSQL Shell --
+SELECT book_name, book_price
+FROM favorite_books
+JOIN book_prices
+ON favorite_books.book_price = book_prices.id;
