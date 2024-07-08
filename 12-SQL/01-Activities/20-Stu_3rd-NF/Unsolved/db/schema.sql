1b55dba24df64a7464715c45f1a1eb9113f5978b
A-MOHAMED14 <amin800@hotmail.co.uk>
2024-07-08 20:04:50 +0100

    Convert Database from 1NF to 3NF

--- 12-SQL/01-Activities/20-Stu_3rd-NF/Unsolved/db/schema.sql
+++ 12-SQL/01-Activities/20-Stu_3rd-NF/Unsolved/db/schema.sql
@@ -11,3 +11,16 @@ CREATE TABLE businesses (
     location_name VARCHAR(255),
     location_manager VARCHAR(255)
 );
+-- ////////////////////////////////////////////////////////////////
+
+CREATE TABLE locations (
+    location_id INTEGER PRIMARY KEY, 
+    location_name VARCHAR(225), 
+    location_manager VARCHAR(225)
+);
+
+CREATE TABLE businesses (
+    business_id INTEGER PRIMARY KEY, 
+    business_name VARCHAR(225),
+    location_id INTEGER FOREIGN KEY REFERENCES locations(location_id)
+);
