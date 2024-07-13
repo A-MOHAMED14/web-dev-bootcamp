c3ec955a0e63fe9d2bbab181675e0655a5aab85e
A-MOHAMED14 <amin800@hotmail.co.uk>
2024-07-13 15:47:19 +0100

    Add function to check if any pair of numbers in an array multiplies to 20

--- 12-SQL/03-Algorithms/01-multiply-into-20/Unsolved/multiply-into-20.js
+++ 12-SQL/03-Algorithms/01-multiply-into-20/Unsolved/multiply-into-20.js
@@ -1,3 +1,15 @@
-const multiplyInto20 = function(arr) {
-  // TODO: Write Your Code Here
+const multiplyInto20 = function (arr) {
+  for (let i = 0; i < arr.length; i++) {
+    const firstNum = arr[i];
+
+    for (let j = 1; j < arr.length; j++) {
+      const secondNum = arr[j];
+
+      if (firstNum * secondNum === 20) {
+        return true;
+      }
+    }
+  }
+
+  return false;
 };
