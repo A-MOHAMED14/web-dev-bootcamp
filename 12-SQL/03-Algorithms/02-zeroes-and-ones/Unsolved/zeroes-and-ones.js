b2604c40234e325afc1f7fabf19bf3799e1daf7c
A-MOHAMED14 <amin800@hotmail.co.uk>
2024-07-13 16:19:23 +0100

    Add function to check for equal number of 0s and 1s in a binary string

--- 12-SQL/03-Algorithms/02-zeroes-and-ones/Unsolved/zeroes-and-ones.js
+++ 12-SQL/03-Algorithms/02-zeroes-and-ones/Unsolved/zeroes-and-ones.js
@@ -2,4 +2,20 @@
 // Return true if there are an equal number of 0s and 1s
 // Else return false
 
-var zeroesAndOnes = function(str) {};
+var zeroesAndOnes = function (str) {
+  const charArr = str.split("");
+
+  let zeroes = 0;
+  let ones = 0;
+
+  charArr.forEach((char) => {
+    if (char === "0") {
+      zeroes++;
+    } else {
+      ones++;
+    }
+  });
+
+  if (zeroes === ones) return true;
+  else return false;
+};
