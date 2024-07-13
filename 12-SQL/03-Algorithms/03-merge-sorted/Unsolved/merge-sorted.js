2b3953940a33c569ea1c23ff651960cd7a0bb54d
A-MOHAMED14 <amin800@hotmail.co.uk>
2024-07-13 16:35:25 +0100

    Add function to merge two sorted arrays into a new sorted array

--- 12-SQL/03-Algorithms/03-merge-sorted/Unsolved/merge-sorted.js
+++ 12-SQL/03-Algorithms/03-merge-sorted/Unsolved/merge-sorted.js
@@ -1,3 +1,9 @@
 // Write code to merge two sorted arrays into a new sorted array
 
-var mergeSorted = function (arr1, arr2) {};
+var mergeSorted = function (arr1, arr2) {
+  const combinedArr = [...arr1, ...arr2];
+
+  const sortedArr = combinedArr.sort((a, b) => a - b);
+
+  return sortedArr;
+};
