fee5ef9776fc4a059daa1efa19e9080f7496938f
A-MOHAMED14 <amin800@hotmail.co.uk>
2024-07-15 11:31:04 +0100

    Implement Routes Which Gracefully Handle Errors and Return Appropriate Status Codes

--- 13-ORM/01-Activities/12-Stu_RESTful-Routes/Unsolved/routes/api/userRoutes.js
+++ 13-ORM/01-Activities/12-Stu_RESTful-Routes/Unsolved/routes/api/userRoutes.js
@@ -1,20 +1,64 @@
-const router = require('express').Router();
-const User = require('../../models/User');
+const router = require("express").Router();
+const User = require("../../models/User");
 
 // TODO: Implement each of the three routes below using `async/await`
 // TODO: Use try/catch to catch errors
 // TODO: Return the appropriate HTTP status codes
 
 // GET a user
-router.get('/:id', async (req, res) => {
+router.get("/:id", async (req, res) => {
+  try {
+    const userData = await User.findByPk(req.params.id);
+
+    if (!userData) {
+      res.status(404).json({ message: "No user with this id!" });
+      return;
+    }
+
+    res.status(200).json(userData);
+  } catch (err) {
+    res.status(500).json({ message: "Internal Server Error" });
+  }
 });
 
 // UPDATE a user
-router.put('/:id', async (req, res) => {
+router.put("/:id", async (req, res) => {
+  try {
+    const userData = await User.update(req.body, {
+      where: {
+        id: req.params.id,
+      },
+    });
+
+    if (!req.body) {
+      res.status(404).json({ message: "No user with this id!" });
+      return;
+    }
+
+    res.status(200).json(userData);
+  } catch (err) {
+    res.status(500).json({ message: "Internal Server Error" });
+  }
 });
 
 // DELETE a user
-router.delete('/:id', async (req, res) => {
+router.delete("/:id", async (req, res) => {
+  try {
+    const userData = await User.destroy({
+      where: {
+        id: req.params.id,
+      },
+    });
+
+    if (!userData) {
+      res.status(404).json({ message: "No user with this id!" });
+      return;
+    }
+
+    res.status(200).json(userData);
+  } catch (err) {
+    res.status(500).json({ message: "Internal Server Error" });
+  }
 });
 
 module.exports = router;
