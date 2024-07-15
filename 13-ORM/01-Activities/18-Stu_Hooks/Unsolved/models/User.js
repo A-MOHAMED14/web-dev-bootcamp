29e77b3bcdc2f8f0143997ea471a17618966a75b
A-MOHAMED14 <amin800@hotmail.co.uk>
2024-07-15 15:21:59 +0100

    Use Hooks to Hash a Password

--- 13-ORM/01-Activities/18-Stu_Hooks/Unsolved/models/User.js
+++ 13-ORM/01-Activities/18-Stu_Hooks/Unsolved/models/User.js
@@ -1,6 +1,6 @@
-const { Model, DataTypes } = require('sequelize');
-const sequelize = require('../config/connection');
-const bcrypt = require('bcrypt');
+const { Model, DataTypes } = require("sequelize");
+const sequelize = require("../config/connection");
+const bcrypt = require("bcrypt");
 
 class User extends Model {}
 
@@ -34,12 +34,28 @@ User.init(
   },
   {
     // TODO: Add hooks here
+    hooks: {
+      beforeCreate: async (newUserData) => {
+        newUserData.password = await bcrypt.hash(newUserData.password, 10);
+        return newUserData;
+      },
+
+      beforeUpdate: async (updatedUserData) => {
+        if (updatedUserData.password) {
+          updatedUserData.password = await bcrypt.hash(
+            updatedUserData.password,
+            10
+          );
+          return updatedUserData;
+        }
+      },
+    },
 
     sequelize,
     timestamps: false,
     freezeTableName: true,
     underscored: true,
-    modelName: 'user',
+    modelName: "user",
   }
 );
 
