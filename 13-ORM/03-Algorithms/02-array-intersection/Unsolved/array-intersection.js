845043a2acc37c16cec9ce3a8d40a990c2216764
A-MOHAMED14 <amin800@hotmail.co.uk>
2024-07-19 17:29:48 +0100

    Add function to find intersection of two arrays with duplicate pairs

--- 13-ORM/03-Algorithms/02-array-intersection/Unsolved/array-intersection.js
+++ 13-ORM/03-Algorithms/02-array-intersection/Unsolved/array-intersection.js
@@ -1,4 +1,19 @@
 // Write code to create a function that accepts two arrays of numbers
 // Return a new array containing the intersecting elements of the arrays
 
-var arrayIntersection = function(arr1, arr2) {};
+var arrayIntersection = function (arr1, arr2) {
+  let intersectionArr = [];
+
+  for (let i = 0; i < arr1.length; i++) {
+    const num1 = arr1[i];
+    for (let j = 0; j < arr2.length; j++) {
+      const num2 = arr2[j];
+
+      if (num1 === num2) {
+        intersectionArr.push(num2);
+      }
+    }
+  }
+
+  return intersectionArr;
+};
