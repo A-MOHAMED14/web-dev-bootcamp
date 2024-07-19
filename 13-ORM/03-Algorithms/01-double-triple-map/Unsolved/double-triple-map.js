653d7c91dcad42d6c4a7eaa140e39397f07e38fb
A-MOHAMED14 <amin800@hotmail.co.uk>
2024-07-19 17:12:00 +0100

    Add function to transform array by doubling evens and tripling odds

--- 13-ORM/03-Algorithms/01-double-triple-map/Unsolved/double-triple-map.js
+++ 13-ORM/03-Algorithms/01-double-triple-map/Unsolved/double-triple-map.js
@@ -2,4 +2,18 @@
 // If a element in the original array is even, the element at the same index in the new array should be double the original element
 // If an element in the original array is odd, the element at the same index of the new array should be triple the original element
 
-var doubleTripleMap = function(arr) {};
+var doubleTripleMap = function (arr) {
+  let newArr = [];
+
+  for (let i = 0; i < arr.length; i++) {
+    const currentNum = arr[i];
+
+    if (currentNum % 2 !== 0) {
+      newArr.push(currentNum * 3);
+    } else {
+      newArr.push(currentNum * 2);
+    }
+  }
+
+  return newArr;
+};
