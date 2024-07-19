be278f3aa9c1349d97346a1c93214deb4b6f9a69
A-MOHAMED14 <amin800@hotmail.co.uk>
2024-07-19 17:36:46 +0100

    Add function to return sorted squares of a sorted integer array

--- 13-ORM/03-Algorithms/03-squares-of-a-sorted-array/Unsolved/squares-of-a-sorted-array.js
+++ 13-ORM/03-Algorithms/03-squares-of-a-sorted-array/Unsolved/squares-of-a-sorted-array.js
@@ -1,3 +1,9 @@
 // Write code to create a function that accepts an array of integers sorted in ascending (increasing) order and returns a new array containing the squares of each number in ascending order
 
-var sortedSquares = function(arr) {};
+var sortedSquares = function (arr) {
+  const sqrArr = arr.map((num) => num ** 2);
+
+  const sortedArr = sqrArr.sort((a, b) => a - b);
+
+  return sortedArr;
+};
