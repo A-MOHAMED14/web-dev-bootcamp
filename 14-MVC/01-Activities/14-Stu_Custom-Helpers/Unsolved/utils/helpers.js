430c97fc2d9054f3b8a10ebe7336210a15e81324
A-MOHAMED14 <amin800@hotmail.co.uk>
2024-07-20 11:44:48 +0100

    Format date as M/D/YYYY

--- 14-MVC/01-Activities/14-Stu_Custom-Helpers/Unsolved/utils/helpers.js
+++ 14-MVC/01-Activities/14-Stu_Custom-Helpers/Unsolved/utils/helpers.js
@@ -6,4 +6,12 @@ module.exports = {
    ? TODO: Create a custom helper 'format_date' that takes in a timestamp,
    ? adds five years to the date, and formats it as M/D/YYYY
   */
+  format_date: (date) => {
+    const timeStamp = new Date(date);
+    const endYear = timeStamp.getFullYear() + 5;
+    const endMonth = timeStamp.getMonth() + 1;
+    const endDay = timeStamp.getDay();
+    const exhibition_end = `${endMonth}/${endDay}/${endYear}`;
+    return exhibition_end;
+  },
 };
