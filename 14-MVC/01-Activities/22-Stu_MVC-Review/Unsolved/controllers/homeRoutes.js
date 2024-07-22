0b02bf49c80780a7ddf87e6e79b1044a4f6947c4
A-MOHAMED14 <amin800@hotmail.co.uk>
2024-07-22 09:51:07 +0100

    Populate User Registry with Database Data

--- 14-MVC/01-Activities/22-Stu_MVC-Review/Unsolved/controllers/homeRoutes.js
+++ 14-MVC/01-Activities/22-Stu_MVC-Review/Unsolved/controllers/homeRoutes.js
@@ -1,8 +1,18 @@
-const router = require('express').Router();
+const router = require("express").Router();
+const User = require("../models/User.js");
 
-router.get('/', async (req, res) => {
+router.get("/", async (req, res) => {
   // TODO: Render template with Sequelize data
-  res.render('homepage');
+  try {
+    const dbUserData = await User.findAll({
+      attributes: { exclude: ["password"] },
+      order: [["name", "ASC"]],
+    });
+    const users = dbUserData.map((user) => user.get({ plain: true }));
+    res.render("homepage", { users });
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 module.exports = router;
