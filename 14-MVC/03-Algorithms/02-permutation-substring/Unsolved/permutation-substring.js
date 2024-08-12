32270202724427268eb979e66c95166f7201151e
A-MOHAMED14 <amin800@hotmail.co.uk>
2024-08-12 11:43:57 +0100

    Add function to check if second string is a substring of any permutation of the first string

--- 14-MVC/03-Algorithms/02-permutation-substring/Unsolved/permutation-substring.js
+++ 14-MVC/03-Algorithms/02-permutation-substring/Unsolved/permutation-substring.js
@@ -2,4 +2,31 @@
 // Return true if the second string is a substring of any permutation of the first string
 // Else return false
 
-var permutationSubstring = function(str, sub) {};
+const permutationSubstring = function (str, sub) {
+  const charCount = {};
+
+  for (let i = 0; i < sub.length; i++) {
+    const char = sub[i];
+
+    if (char in charCount) {
+      charCount[char]++;
+    } else {
+      charCount[char] = 1;
+    }
+  }
+
+  for (let i = 0; i < str.length; i++) {
+    const char = str[i];
+    if (char in charCount) {
+      charCount[char] -= 1;
+    }
+  }
+
+  for (const key in charCount) {
+    if (charCount[key] > 0) {
+      return false;
+    }
+  }
+
+  return true;
+};
