19996904cdce4839f9da4c17d4e5927fc5187777
A-MOHAMED14 <amin800@hotmail.co.uk>
2024-08-12 12:00:44 +0100

    Implement function to verify if all words in concert flyer can be found in the magazine

--- 14-MVC/03-Algorithms/03-concert-flyer/Unsolved/concert-flyer.js
+++ 14-MVC/03-Algorithms/03-concert-flyer/Unsolved/concert-flyer.js
@@ -1,3 +1,34 @@
 // Write a function that takes two strings and returns true if every word found in the second string is present in the first string. You will be checking for both words and their frequency. Assume you'll need to worry about casing, but the strings won't contain any punctuation. Assume neither string will be empty
 
-var concertFlyer = function (magazine, flyer) {};
+const concertFlyer = function (magazine, flyer) {
+  const magazineArr = magazine.split(" ");
+  const flyerArr = flyer.split(" ");
+
+  const wordCount = {};
+
+  for (let i = 0; i < flyerArr.length; i++) {
+    const word = flyerArr[i];
+
+    if (word in wordCount) {
+      wordCount[word]++;
+    } else {
+      wordCount[word] = 1;
+    }
+  }
+
+  for (let i = 0; i < magazineArr.length; i++) {
+    const word = magazineArr[i];
+
+    if (word in wordCount) {
+      wordCount[word]--;
+    }
+  }
+
+  for (const key in wordCount) {
+    if (wordCount[key] > 0) {
+      return false;
+    }
+  }
+
+  return true;
+};
