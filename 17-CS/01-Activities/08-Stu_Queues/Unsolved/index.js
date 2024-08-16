10b63a8852a236611dce9643a3ceed843aaa98db
A-MOHAMED14 <amin800@hotmail.co.uk>
2024-08-16 14:54:12 +0100

    Implement a Queue by using a Class

--- 17-CS/01-Activities/08-Stu_Queues/Unsolved/index.js
+++ 17-CS/01-Activities/08-Stu_Queues/Unsolved/index.js
@@ -1,6 +1,16 @@
 // create the Queue class with two methods to remove and add an element
 class Queue {
-  
+  constructor(container = []) {
+    this.container = container;
+  }
+
+  addToQueue(element) {
+    return this.container.push(element);
+  }
+
+  removeFromQueue() {
+    return this.container.shift();
+  }
 }
 
 module.exports = Queue;
