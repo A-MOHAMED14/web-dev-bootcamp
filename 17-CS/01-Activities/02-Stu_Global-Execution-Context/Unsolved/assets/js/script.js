7a6884b41dbb35e04a7a23a15646a0dd4fd573d3
A-MOHAMED14 <amin800@hotmail.co.uk>
2024-08-16 11:11:44 +0100

    Add Comments to the Code Execution Process

--- 17-CS/01-Activities/02-Stu_Global-Execution-Context/Unsolved/assets/js/script.js
+++ 17-CS/01-Activities/02-Stu_Global-Execution-Context/Unsolved/assets/js/script.js
@@ -1,25 +1,26 @@
 // 1) Where is carNoise stored?
-//
+// global execution context
 const carNoise = 'Honk';
 // 2) Where is goFast stored?
-// 
+// global execution context
 const goFast = speed => {
   // 4) When is speed assigned a value? Where is this value stored?
-  // 
+  // When goFast function is invoked with an argument.
+  // It is stored in the function execution context
   
   // 5) Where is makeNoise stored?
-  //
+  // Function execution context of goFast
   const makeNoise = sound => {
     console.log(`My speed is at ${speed}, time to ${sound}`);
   }
 
   // 6) What happens in the following statement?
-  // 
+  // makeNoise function is placed on the call stack, creating a new function execution context
   makeNoise(carNoise);
 }
 
 // 3) What happens in the following statement?
-// 
+// Confirm is a web API, once resolved, goFast function is placed is in the callback queue, then onto the call stack to be executed.
 if(confirm("Do you want to go fast?")) {
   goFast(80);
 }
