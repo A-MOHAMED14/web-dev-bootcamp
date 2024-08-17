27d5fe917579851df8227d860d07456c1494bf08
A-MOHAMED14 <amin800@hotmail.co.uk>
2024-08-17 11:38:42 +0100

    Add Comments to Implementation of Using Closures for Event Delegation

--- 17-CS/01-Activities/20-Stu_Event-Delegation-Handling/Unsolved/assets/js/script.js
+++ 17-CS/01-Activities/20-Stu_Event-Delegation-Handling/Unsolved/assets/js/script.js
@@ -1,20 +1,25 @@
-const buttons = document.getElementsByTagName('button');
+const buttons = document.getElementsByTagName("button");
 
 const clickHandler = function () {
-// TODO: Add a comment describing the purpose of this variable.
+  // TODO: Add a comment describing the purpose of this variable.
+  // Create a count variable which can be used by the inner function
   let count = 0;
 
-  // TODO: Add a comment describing what is happening. 
+  // TODO: Add a comment describing what is happening.
+  // returning an inner function which creates closure
   return function () {
-  
-   // TODO: Add a comment describing how this variable is being scoped. 
+    // TODO: Add a comment describing how this variable is being scoped.
+    // Increment the scoped variable
     count++;
-   // TODO: Add a comment describing how we are using the 'count' variable.
+    // TODO: Add a comment describing how we are using the 'count' variable.
+    // Use a template literal to display the count variable onto the button element.
+
     this.textContent = `Clicks: ${count}`;
   };
 };
 
 // TODO: Add a comment describing the purpose of this loop.
+// Adding a click event to each of the buttons rather than adding it to the container which contains all the buttons
 for (let i = 0; i < buttons.length; i++) {
-  buttons[i].addEventListener('click', clickHandler());
+  buttons[i].addEventListener("click", clickHandler());
 }
