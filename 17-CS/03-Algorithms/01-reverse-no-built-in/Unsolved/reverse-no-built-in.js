ae993af4258c6f023ba31e8bfa1a50c763d87584
A-MOHAMED14 <amin800@hotmail.co.uk>
2024-08-18 12:30:21 +0100

    Create a function which returns a new string with all of the characters in the string reversed without using the built-in reverse method

--- 17-CS/03-Algorithms/01-reverse-no-built-in/Unsolved/reverse-no-built-in.js
+++ 17-CS/03-Algorithms/01-reverse-no-built-in/Unsolved/reverse-no-built-in.js
@@ -1,4 +1,15 @@
 // Write code to return a new string with all of the characters in `str` reversed.
 // You may NOT use the built-in reverse method
 
-const reverse = (str) => {};
+const reverse = (str) => {
+  console.log(str);
+
+  let reversedStr = "";
+
+  for (let i = str.length - 1; i >= 0; i--) {
+    const currentChar = str[i];
+    reversedStr = reversedStr + currentChar;
+  }
+
+  return reversedStr;
+};
