20d544b36c733fb5f6fefa32144585f7155bc143
A-MOHAMED14 <amin800@hotmail.co.uk>
2024-08-18 10:17:11 +0100

    Implement a Linear Search Algorithm for multiple elements

--- 17-CS/01-Activities/22-Stu_Linear-Search/Unsolved/index.js
+++ 17-CS/01-Activities/22-Stu_Linear-Search/Unsolved/index.js
@@ -1,13 +1,30 @@
 const arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 7];
 
 // TODO: Complete the linearSearch function below.
-const linearSearch = (array, element) => {
+// const linearSearch = (array, element) => {
+//   for (let i = 0; i < array.length; i++) {
+//     const currentNum = array[i];
+
+//     if (currentNum === element) return i;
+//   }
+//   return -1;
+// };
+
+const linearSearchMultipleElements = (array, element) => {
   for (let i = 0; i < array.length; i++) {
+    let matchedIndexes = [];
     const currentNum = array[i];
 
-    if (currentNum === element) return i;
+    if (currentNum === element) {
+      matchedIndexes.push[i];
+    }
   }
-  return -1;
+
+  if (matchedIndexes.length === 0) {
+    return "No matches found";
+  } else {
+    return matchedIndexes;
+  }
 };
 
-module.exports = { linearSearch };
+module.exports = { linearSearch, linearSearchMultipleElements };
