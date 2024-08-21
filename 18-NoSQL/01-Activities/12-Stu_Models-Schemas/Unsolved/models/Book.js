5cba66b672c500de8dcee2ccd577106f1763ef54
A-MOHAMED14 <amin800@hotmail.co.uk>
2024-08-21 10:51:07 +0100

    Add Comments to Implementation of Models and Schemas Using Mongoose

--- 18-NoSQL/01-Activities/12-Stu_Models-Schemas/Unsolved/models/Book.js
+++ 18-NoSQL/01-Activities/12-Stu_Models-Schemas/Unsolved/models/Book.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose');
 
 // TODO: Add a comment describing the functionality of the code below
+// Defines the properties, datatypes and constraints that the models will use.
 const bookSchema = new mongoose.Schema({
   title: { type: String, required: true },
   author: { type: String, required: false },
@@ -12,11 +13,13 @@ const bookSchema = new mongoose.Schema({
 });
 
 // TODO: Add a comment describing the functionality of the code below
+// Creates a Book model which uses the bookSchema schema
 const Book = mongoose.model('Book', bookSchema);
 
 const handleError = (err) => console.error(err);
 
 // TODO: Add a comment describing the functionality of the code below
+// Creates a document using the Book model
 Book
   .create({
     title: 'Diary of Anne Frank',
@@ -31,6 +34,7 @@ Book
 
 // TODO: Add a comment describing the difference between this instance being created
 // and the instance that was created above
+// Creates a document using only two propoerties, the required title and optional author properties  
 Book
   .create({
     title: 'Oh the Places You Will Go!',
@@ -41,6 +45,7 @@ Book
 
 // TODO: Add a comment describing the difference between this instance being created
 // and the instance that was created above
+// Creates a document using the required title property
 Book.create({ title: 'Harold and the Purple Crayon' })
   .then(result => console.log('Created new document', result))
   .catch(err => handleError(err));
