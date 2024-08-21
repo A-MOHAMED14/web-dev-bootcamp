e886649a6e1d7504e7ea97efc136e470d52bab12
A-MOHAMED14 <amin800@hotmail.co.uk>
2024-08-21 13:13:59 +0100

    Implement Subdocuments in a Mongoose Schema

--- 18-NoSQL/01-Activities/18-Stu_Subdocuments/Unsolved/models/Library.js
+++ 18-NoSQL/01-Activities/18-Stu_Subdocuments/Unsolved/models/Library.js
@@ -1,17 +1,34 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 // TODO: Define a new schema named `bookSchema` for the subdocument
 
+const bookSchema = new mongoose.Schema({
+  title: { type: String, required: true },
+  price: Number,
+});
+
 const librarySchema = new mongoose.Schema({
   name: { type: String, required: true },
-  //
   // TODO: Add the `books` subdocument to the parent document as an array
-  //
+  books:[ bookSchema],
   lastAccessed: { type: Date, default: Date.now },
 });
 
 // TODO: Create a model named `Library`
+const Library = new mongoose.model("Library", librarySchema);
 
 // TODO: Create a new instance of the model including the subdocuments
+const bookData = [
+  { title: "The shining", price: 15 },
+  { title: "Crime & Punishment", price: 25 },
+  { title: "1984", price: 7 },
+];
+
+Library.create({
+  name: "Books",
+  books: bookData,
+})
+  .then((data) => console.log(data))
+  .catch((err) => console.error(err));
 
 module.exports = Library;
