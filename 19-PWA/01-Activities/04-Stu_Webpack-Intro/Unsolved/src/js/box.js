250b01d962b57a6e4dcf9e58a9c147cdf758de7c
A-MOHAMED14 <amin800@hotmail.co.uk>
2024-08-27 12:30:01 +0100

    Add Comments to Implementation of webpack Setup

--- 19-PWA/01-Activities/04-Stu_Webpack-Intro/Unsolved/src/js/box.js
+++ 19-PWA/01-Activities/04-Stu_Webpack-Intro/Unsolved/src/js/box.js
@@ -1,10 +1,11 @@
 // TODO: Add a comment explaining the role of `export` in this function `boxClick()`
+// Exports boxClick function using an export statement so that we can import it to our index.js file
 
 export const boxClick = () => {
-  const box = document.getElementById('box');
-  if (box.style.backgroundColor === 'blue') {
-    box.style.backgroundColor = 'yellow';
+  const box = document.getElementById("box");
+  if (box.style.backgroundColor === "blue") {
+    box.style.backgroundColor = "yellow";
   } else {
-    box.style.backgroundColor = 'blue';
+    box.style.backgroundColor = "blue";
   }
 };
--- 19-PWA/01-Activities/04-Stu_Webpack-Intro/Unsolved/src/js/index.js
+++ 19-PWA/01-Activities/04-Stu_Webpack-Intro/Unsolved/src/js/index.js
@@ -1,4 +1,6 @@
 // TODO: Add a comment explaining role of the index.js file and import statements
+// Index.js is the entry point file.
+// Imports other modules and assets into this file.
 
 import { boxClick } from './box';
 import { headerClick } from './header';
