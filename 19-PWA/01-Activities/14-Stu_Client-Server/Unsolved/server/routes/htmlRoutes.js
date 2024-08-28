827771c4f03612c66e2a650a8cbdf6913607a6c1
A-MOHAMED14 <amin800@hotmail.co.uk>
2024-08-28 13:19:32 +0100

    Add Comments to Implementation of Client-Server Model

--- 19-PWA/01-Activities/14-Stu_Client-Server/Unsolved/server/routes/htmlRoutes.js
+++ 19-PWA/01-Activities/14-Stu_Client-Server/Unsolved/server/routes/htmlRoutes.js
@@ -1,6 +1,7 @@
 const path = require('path');
 
 // TODO: Add comment explaining role of route below
+// Any GET requests made to the root route "/", it will serve the index.html file in the clients dist folder
 module.exports = (app) =>
   app.get('/', (req, res) =>
     res.sendFile(path.join(__dirname, '../client/dist/index.html'))
--- 19-PWA/01-Activities/14-Stu_Client-Server/Unsolved/server/server.js
+++ 19-PWA/01-Activities/14-Stu_Client-Server/Unsolved/server/server.js
@@ -4,10 +4,12 @@ const app = express();
 const PORT = process.env.PORT || 3001;
 
 // TODO: Add comment on role of code below
+// Serves static files within the clients dist folder
 app.use(express.static('../client/dist'));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
+// Requires the route which serves the client with the index.html file within the clients dist folder
 require('./routes/htmlRoutes')(app);
 
 app.listen(PORT, () => console.log(`Now listening on port: ${PORT}`));
