d5a516b975a91669f045f703dd8fbf21d45e3871
A-MOHAMED14 <amin800@hotmail.co.uk>
2024-08-29 11:03:00 +0100

    Add Comments to Implementation of IndexedDB

--- 19-PWA/01-Activities/22-Stu_IndexedDB/Unsolved/assets/js/database.js
+++ 19-PWA/01-Activities/22-Stu_IndexedDB/Unsolved/assets/js/database.js
@@ -1,17 +1,21 @@
 import { openDB } from 'idb';
 
+// Function used to start up the database
 const initdb = async () =>
 
 // TODO: Add a comment explaining what this method does
-
+// Creates a database called todos, using version 1
   openDB('todos', 1, {
     // TODO: Add a comment explaining the functionality of this method:
+    // Checks if an object store called todos exists within the database
     upgrade(db) {
       if (db.objectStoreNames.contains('todos')) {
         console.log('todos database already exists');
         return;
       }
       // TODO: Add a comment explaining what we're doing with the object store:
+      // Creates an object store called todos within the todos database
+      // Also creates an id key which auto-increments
       db.createObjectStore('todos', { keyPath: 'id', autoIncrement: true });
       console.log('todos database created');
     },
