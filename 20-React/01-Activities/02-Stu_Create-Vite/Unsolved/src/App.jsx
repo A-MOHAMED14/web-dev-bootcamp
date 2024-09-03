602a260796885aa79a6368e8216cd74959589f96
A-MOHAMED14 <amin800@hotmail.co.uk>
2024-09-03 11:08:38 +0100

    Add Comments That Describe the Functionality of a React App

--- 20-React/01-Activities/02-Stu_Create-Vite/Unsolved/src/App.jsx
+++ 20-React/01-Activities/02-Stu_Create-Vite/Unsolved/src/App.jsx
@@ -1,7 +1,9 @@
 // TODO: Add a comment explaining what this import statement is doing
+// Imports the HelloReact compenent from the components folder
 import HelloReact from './components/HelloReact';
 
 // TODO: Add a comment explaining the purpose of the App component
+// The App component returns the HelloReact component so that it can be rendered
 function App() {
   return <HelloReact />;
 }
--- 20-React/01-Activities/02-Stu_Create-Vite/Unsolved/src/components/HelloReact.jsx
+++ 20-React/01-Activities/02-Stu_Create-Vite/Unsolved/src/components/HelloReact.jsx
@@ -1,4 +1,8 @@
 // TODO: Add a comment explaining what a react component is
+// A React component is like a building block for an application. It's a small piece that you can create, reuse, and combine to build the whole application.
+// We can pass data to these components and even render other components inside other components.
+// The `HelloReact` component is a child of the `App` component and is responsible for rendering the "Hello World" header.
+
 function HelloReact() {
   const text = 'some text';
 
