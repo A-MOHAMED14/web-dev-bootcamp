585854ffee240b57ea1f99899972a866045be277
A-MOHAMED14 <amin800@hotmail.co.uk>
2024-09-05 11:53:07 +0100

    render the SearchBar component and format the rendered HTML using the pretty package before saving it as a snapshot

--- 20-React/01-Activities/26-Stu_Testing/Unsolved/src/tests/SearchBar.test.jsx
+++ 20-React/01-Activities/26-Stu_Testing/Unsolved/src/tests/SearchBar.test.jsx
@@ -1,17 +1,15 @@
-import pretty from 'pretty';
-import { render } from '@testing-library/react';
-import SearchBar from '../components/SearchBar';
+import pretty from "pretty";
+import { render } from "@testing-library/react";
+import SearchBar from "../components/SearchBar";
 
-describe('SearchBar', () => {
+describe("SearchBar", () => {
   // In this example, we render the SearchBar component and format the rendered HTML using the pretty package before saving it as a snapshot
-  it('should render and match snapshot', () => {
-    act(() => {
-      // Render the component
-      render(<SearchBar />, container);
-    });
+  it("should render and match snapshot", () => {
+    // Render the component
+    render(<SearchBar />);
 
     // Format the rendered output
-    const html = pretty(container.innerHTML);
+    const html = pretty(document.querySelector(".search-bar").outerHTML);
 
     // Verify the output
     expect(html).toMatchSnapshot();
