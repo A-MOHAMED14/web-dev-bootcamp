66f18d5db56be60dda7fea9068ead3459a51b574
A-MOHAMED14 <amin800@hotmail.co.uk>
2024-09-05 09:16:42 +0100

    Add Comments to Implementation of Conditional Rendering

--- 20-React/01-Activities/22-Stu_Conditional-Rendering/Unsolved/src/components/NavTabs.jsx
+++ 20-React/01-Activities/22-Stu_Conditional-Rendering/Unsolved/src/components/NavTabs.jsx
@@ -1,4 +1,5 @@
 // TODO: Add a comment explaining how we are able to extract the key value pairs from props
+// By destructing the props object
 
 function NavTabs({ currentPage, handlePageChange }) {
   return (
@@ -6,10 +7,13 @@ function NavTabs({ currentPage, handlePageChange }) {
       <li className="nav-item">
         <a
           href="#home"
-          onClick={() => handlePageChange('Home')}
+          onClick={() => handlePageChange("Home")}
           //*  TODO: BONUS: Add a comment explaining what kind of operator this is and what it is checking for
+          // Conditional ternary operator.
+          // Checks if the current page is Home.
+          // If it is, we set the current page to nav-link-active, otherwise we set it to nav-link
 
-          className={currentPage === 'Home' ? 'nav-link active' : 'nav-link'}
+          className={currentPage === "Home" ? "nav-link active" : "nav-link"}
         >
           Home
         </a>
@@ -17,10 +21,12 @@ function NavTabs({ currentPage, handlePageChange }) {
       <li className="nav-item">
         <a
           href="#about"
-          onClick={() => handlePageChange('About')}
+          onClick={() => handlePageChange("About")}
           //  TODO: Add a comment explaining what this logic is doing
+          // Checks if the current page is About.
+          // If it is, we set the current page to nav-link-active, otherwise we set it to nav-link
 
-          className={currentPage === 'About' ? 'nav-link active' : 'nav-link'}
+          className={currentPage === "About" ? "nav-link active" : "nav-link"}
         >
           About
         </a>
@@ -28,10 +34,12 @@ function NavTabs({ currentPage, handlePageChange }) {
       <li className="nav-item">
         <a
           href="#blog"
-          onClick={() => handlePageChange('Blog')}
+          onClick={() => handlePageChange("Blog")}
           //  TODO: Add a comment explaining what this logic is doing
+          // Checks if the current page is Blog.
+          // If it is, we set the current page to nav-link-active, otherwise we set it to nav-link
 
-          className={currentPage === 'Blog' ? 'nav-link active' : 'nav-link'}
+          className={currentPage === "Blog" ? "nav-link active" : "nav-link"}
         >
           Blog
         </a>
@@ -39,10 +47,11 @@ function NavTabs({ currentPage, handlePageChange }) {
       <li className="nav-item">
         <a
           href="#contact"
+          onClick={() => handlePageChange("Contact")}
           //  TODO: Add a comment explaining what this logic is doing
-
-          onClick={() => handlePageChange('Contact')}
-          className={currentPage === 'Contact' ? 'nav-link active' : 'nav-link'}
+          // Checks if the current page is Contact.
+          // If it is, we set the current page to nav-link-active, otherwise we set it to nav-link
+          className={currentPage === "Contact" ? "nav-link active" : "nav-link"}
         >
           Contact
         </a>
--- 20-React/01-Activities/22-Stu_Conditional-Rendering/Unsolved/src/components/PortfolioContainer.jsx
+++ 20-React/01-Activities/22-Stu_Conditional-Rendering/Unsolved/src/components/PortfolioContainer.jsx
@@ -9,6 +9,7 @@ export default function PortfolioContainer() {
   const [currentPage, setCurrentPage] = useState('Home');
 
   // TODO: Add a comment describing the functionality of this method
+  // Conditionally returns a component to render depending on the value of current page
   const renderPage = () => {
     if (currentPage === 'Home') {
       return <Home />;
@@ -27,8 +28,10 @@ export default function PortfolioContainer() {
   return (
     <div>
       {/* // TODO: Add a comment describing what we are passing as props */}
+      {/* // The currentPage state variable and the handlePageChange method which updates the state are being passed to the NavTabs component as props */}
       <NavTabs currentPage={currentPage} handlePageChange={handlePageChange} />
       {/* // TODO: Add a comment explaining what is happening on the following line */}
+      {/* // Invoking renderPage() which will return a component for render */}
       <main className="mx-3">{renderPage()}</main>
     </div>
   );
