a3293e754bfbf87df113e7b0b5e107aee3f9993f
A-MOHAMED14 <amin800@hotmail.co.uk>
2024-09-12 13:02:50 +0100

    Add Comments to Implementation of JWT Signing

--- 21-MERN/01-Activities/22-Stu_Sign-JWT/Unsolved/schemas/resolvers.js
+++ 21-MERN/01-Activities/22-Stu_Sign-JWT/Unsolved/schemas/resolvers.js
@@ -21,26 +21,30 @@ const resolvers = {
   Mutation: {
     // TODO: Add comments to each line of code below to describe the functionality below
     addUser: async (parent, args) => {
+      // A new user is created and their info is stored in the database
       const user = await User.create(args);
+      // Then, the user is logged in and a JWT is created/signed using the users info
       const token = signToken(user);
-
+      // Lasty, an Auth object is returned which consists of the created/signed token and the user's info
       return { token, user };
     },
     // TODO: Add comments to each line of code below to describe the functionality below
     login: async (parent, { email, password }) => {
+      // A user is looked up using the provided email. We use email to lookup a user as this field is unique.
       const user = await User.findOne({ email });
-
+      // If there is no user with the provided email, an AuthenticationError is returned/thrown stating so
       if (!user) {
         throw AuthenticationError
       }
-
+      // If there is a user found, execute the isCorrectPassword instance method and check if the correct password was provided
       const correctPw = await user.isCorrectPassword(password);
-
+      // If they do not match, an AuthenticationError is returned stating so
       if (!correctPw) {
         throw AuthenticationError
       }
-
+      // If the email and password are correct, then a JWT is created/signed using the users info
       const token = signToken(user);
+      // Lasty, an Auth object is returned which consists of the created/signed token and the user's info
       return { token, user };
     },
     addThought: async (parent, { thoughtText, thoughtAuthor }) => {
